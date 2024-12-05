997f2ae759683512da8c3371f5c48ae5be73cc40
Defoltini <nesti-1998@mail.ru>
2024-12-06 00:06:23 +0300

    added test

--- src/test/java/CheckCodeForJUnit.java
+++ src/test/java/CheckCodeForJUnit.java
@@ -1,2 +1,18 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
 public class CheckCodeForJUnit {
+    @Test
+    void checkTextOnPage() {
+        // Открыть страницу
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-body").$(".markdown-body").shouldHave(Condition.text("Soft assertions"));
+            $(byText("Soft assertions")).click();
+        $(".repository-content").shouldHave(Condition.text("Using JUnit5 extend test class:"));
+    }
 }
