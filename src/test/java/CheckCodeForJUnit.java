4cc533574536cab98bc8beacab89239f2139916b
Defoltini <nesti-1998@mail.ru>
2024-12-08 22:04:47 +0300

    added string text

--- src/test/java/CheckCodeForJUnit.java
+++ src/test/java/CheckCodeForJUnit.java
@@ -8,11 +8,23 @@ import static com.codeborne.selenide.Selenide.*;
 public class CheckCodeForJUnit {
     @Test
     void checkTextOnPage() {
-        // Открыть страницу
         open("https://github.com/selenide/selenide");
         $("#wiki-tab").click();
         $("#wiki-body").$(".markdown-body").shouldHave(Condition.text("Soft assertions"));
             $(byText("Soft assertions")).click();
-        $(".repository-content").shouldHave(Condition.text("Using JUnit5 extend test class:"));
+            //Ввожу переменнную text с необходимым текстом для проверки
+            String text = "@ExtendWith({SoftAssertsExtension.class})\n" +
+                    "class Tests {\n" +
+                    "  @Test\n" +
+                    "  void test() {\n" +
+                    "    Configuration.assertionMode = SOFT;\n" +
+                    "    open(\"page.html\");\n" +
+                    "\n" +
+                    "    $(\"#first\").should(visible).click();\n" +
+                    "    $(\"#second\").should(visible).click();\n" +
+                    "  }\n" +
+                    "}";
+        $(".repository-content").shouldHave(Condition.text(text));
+
     }
 }
